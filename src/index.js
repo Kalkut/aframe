e56482a35b336a51823e12902c0b6b138ca1eab3
Kalkut <berenger.makita@gmail.com>
2017-11-16 13:26:08 +0100

    Update three-bmfont-text dependancy to add a Content Security Policy Fix

--- src/index.js
+++ src/index.js
@@ -83,7 +83,7 @@ require('./core/a-mixin');
 require('./extras/components/');
 require('./extras/primitives/');
 
-console.log('A-Frame Version: 0.7.0 (Date 2017-12-27, Commit #ce910c9)');
+console.log('A-Frame Version: 0.7.0 (Date 2018-01-09, Commit #752a14a4)');
 console.log('three Version:', pkg.dependencies['three']);
 console.log('WebVR Polyfill Version:', pkg.dependencies['webvr-polyfill']);
 
